9aaf47dc76887a4e11f3b10e482a9cd94da7e1e2
sono <rhj080471@gmail.com>
2024-09-06 01:26:08 +0900

    StyleSheet를 이용한 스타일링 강의 수강

--- app/(tabs)/screens/MainScreen.tsx
+++ app/(tabs)/screens/MainScreen.tsx
@@ -1,16 +1,24 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 
 const MainScreen = () => {
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={"default"} />
-      <Text>Todo App</Text>
-      <View>
-        <Text>할 일</Text>
+    <SafeAreaView style={styles.container}>
+      {/* <StatusBar barStyle={"default"} /> */}
+      <Text style={styles.pageTitle}>Todo App</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할 일</Text>
       </View>
-      <View>
-        <Text>완료된 일</Text>
+      <View style={styles.separator}></View>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
       </View>
     </SafeAreaView>
   );
@@ -18,4 +26,32 @@ const MainScreen = () => {
 
 export default MainScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // paddingTop: Platform.OS === "android" ? 20 : 0,
+    backgroundColor: "f7f8fa",
+  },
+  pageTitle: {
+    marginBottom: 35,
+    paddingHorizontal: 15,
+    fontSize: 54,
+    fontWeight: "600",
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: "500",
+  },
+  separator: {
+    marginHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0,0,0,0.2)",
+  },
+});
