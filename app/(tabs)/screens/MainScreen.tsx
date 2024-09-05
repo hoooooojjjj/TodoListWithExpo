29ffc8d5b2fd39f17f0a6339374c22c18546f97a
sono <rhj080471@gmail.com>
2024-09-06 01:02:04 +0900

    Core Components를 이용한 UI 작성하기 강의 수강

--- app/(tabs)/screens/MainScreen.tsx
+++ app/(tabs)/screens/MainScreen.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const MainScreen = () => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar barStyle={"default"} />
+      <Text>Todo App</Text>
+      <View>
+        <Text>할 일</Text>
+      </View>
+      <View>
+        <Text>완료된 일</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
