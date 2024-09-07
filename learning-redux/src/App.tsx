9f476d8b99a6aa103663b28756d138b98b81922c
sono <rhj080471@gmail.com>
2024-09-07 12:34:24 +0900

    카운터 구현

--- learning-redux/src/App.tsx
+++ learning-redux/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increase}>+</button>
+      <p>{count}</p>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
