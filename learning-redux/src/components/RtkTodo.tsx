9afc3c600d53f516fff2dd0663553342692dd453
sono <rhj080471@gmail.com>
2024-09-09 23:52:16 +0900

    리덕스 툴킷 완료

--- learning-redux/src/components/RtkTodo.tsx
+++ learning-redux/src/components/RtkTodo.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../reducer/rtkStore";
 import { addTodo } from "../reducer/rtkReducer";
 
 function RtkTodo() {
+  const [text, setText] = useState<string>("");
+
   const todo: {
     id: number;
     todo: string;
@@ -12,10 +14,18 @@ function RtkTodo() {
   const TodoDispatch = useDispatch();
 
   const addTodos = () => {
-    TodoDispatch(addTodo({ id: Date.now(), todo: "text" }));
+    TodoDispatch(addTodo({ id: Date.now(), todo: text }));
+    setText("");
   };
   return (
     <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      ></input>
       <button onClick={addTodos}>추가</button>
       {todo.map((todo) => (
         <div key={todo.id}>{todo.todo}</div>
