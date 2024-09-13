3f580e59581046c94e742958bcd9d933c92792b1
sono <rhj080471@gmail.com>
2024-09-14 01:05:04 +0900

    Bug : 'NavigationContainer' 중첩 오류

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
 import { store } from "@/redux/store";
 import MainScreen from "./screens/MainScreen";
 import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function HomeScreen() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Main" component={MainScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />{" "}
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
